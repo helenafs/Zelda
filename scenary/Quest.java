5f0893678e1dd89ec3b6bf8e90ce6fa648c999b4
Helena Santos <38816678+helenafs@users.noreply.github.com>
2023-04-17 11:55:54 +0200

    Update Quest.java

--- scenary/Quest.java
+++ scenary/Quest.java
@@ -6,6 +6,10 @@ import zelda.Zelda;
 
 import com.golden.gamedev.object.PlayField;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Quest extends PlayField {
     
     private Zelda game;
@@ -17,18 +21,38 @@ public class Quest extends PlayField {
     public Quest(Zelda game) {
         super();
         this.game = game;
-        this.boards = new Board[1][1];
+        this.boards = new Board[2][2];
         this.initRessources();
     }
 
     private void initRessources() {
         this.menu = new QuestMenu(this.game);
         
-        
+        //lire les fichiers
+        String fileName = "myfile.txt"; // replace with your file name
+        int charIndex = 10; // replace with the index of the character you want
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            int c;
+            int currentIndex = 0;
+            while ((c = br.read()) != -1) {
+                if (currentIndex == charIndex) {
+                    char myChar = (char) c;
+                    System.out.println("The character at index " + charIndex + " is " + myChar);
+                    break; // exit loop once character is found
+                }
+                currentIndex++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
         // Board (0, 0)
         Board b00 = new Board(this.game, 0, 0);
         
-        b00.add(new Rock(this.game, Rock.Kind.GREEN_PLAIN));
+        //b00.add(new Rock(this.game, Rock.Kind.GREEN_PLAIN));
+        b00.add(new Floor(this.game, Floor.Color.SAND));
         b00.add(new Rock(this.game, Rock.Kind.GREEN_PLAIN));
         b00.add(new Rock(this.game, Rock.Kind.GREEN_PLAIN));
         b00.add(new Rock(this.game, Rock.Kind.GREEN_PLAIN));
