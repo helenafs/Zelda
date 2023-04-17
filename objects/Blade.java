80f6627a37ef73c5a31116065aea176fedca93f0
kaoutar-ny <79809553+kaoutar-ny@users.noreply.github.com>
2023-04-17 16:38:19 +0200

    Add files via upload

--- objects/Blade.java
+++ objects/Blade.java
@@ -1,6 +1,16 @@
 package zelda.objects;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import com.golden.gamedev.object.Sprite;
+
+import zelda.Zelda;
+import zelda.scenary.AbstractTile;
+import zelda.scenary.Board;
 
 public class Blade extends Sprite {
     
@@ -10,4 +20,60 @@ public class Blade extends Sprite {
         SILVER,
         MAGICAL
     }
+    
+    private Kind kind;
+    private Zelda damage;
+    public Sprite img;
+    
+    public Blade(Zelda game, Kind silver) {
+        this.kind = silver;
+        this.damage = game;
+        
+        
+        
+        // Initialise l'image en fonction du type d'arme
+    
+        switch (kind) {
+            case WOOD:
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBE.gif")); 
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBN.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBW.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBS.gif"));
+                break;
+            case SILVER:
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAE.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAN.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAW.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAS.gif"));	
+                break;
+            case MAGICAL:
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBE.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBN.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBW.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBS.gif"));
+                break;
+            default:
+                // Si le type d'arme n'est pas valide, utilise une image par défaut
+            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAE.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAN.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAW.gif"));
+            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAS.gif"));
+                break;
+        }
+        
+     }
+    
+    
+
+	public Kind getKind() {
+        return kind;
+    }
+    
+    public Zelda getDamage() {
+        return damage;
+    }
+
+
+	
+	
 }
