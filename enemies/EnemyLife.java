3c1cd6d7c247197547945b96740ae5c35a9cbb9d
Helena Santos <38816678+helenafs@users.noreply.github.com>
2023-04-19 14:29:27 +0200

    Add files via upload

---
+++ enemies/EnemyLife.java
@@ -0,0 +1,36 @@
+package zelda.enemies;
+
+import zelda.LinkCollisionManager;
+
+public class EnemyLife {
+	private int currentHealth;
+    private int maxHealth;
+    
+    public EnemyLife(int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+    
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+    
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+    
+    public void takeDamage(int damage) {
+        currentHealth -= damage;
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        }
+    }
+    
+    public boolean isDead() {
+        return currentHealth <= 0;
+    }
+}
