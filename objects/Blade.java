04c8cce6c5ac79a47fd478f0cdf568ca7d78c6a1
Helena Santos <38816678+helenafs@users.noreply.github.com>
2023-04-19 15:25:41 +0200

    Add files via upload

--- objects/Blade.java
+++ objects/Blade.java
@@ -22,42 +22,44 @@ public class Blade extends Sprite {
     }
     
     private Kind kind;
-    private Zelda damage;
+    private Zelda game;
     public Sprite img;
+    int damage;
     
-    public Blade(Zelda game, Kind silver) {
+    
+    public Blade(Zelda game, Kind silver, int damage) {
         this.kind = silver;
-        this.damage = game;
-        
+        this.game = game;
+        this.damage = damage;
         
         
         // Initialise l'image en fonction du type d'arme
     
         switch (kind) {
             case WOOD:
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBE.gif")); 
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBN.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBW.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OWBS.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OWBE.gif")); 
+            	this.setImage(this.game.getImage("res/sprites/Objects/OWBN.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OWBW.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OWBS.gif"));
                 break;
             case SILVER:
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAE.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAN.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAW.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OSAS.gif"));	
+            	this.setImage(this.game.getImage("res/sprites/Objects/OSAE.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OSAN.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OSAW.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OSAS.gif"));	
                 break;
             case MAGICAL:
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBE.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBN.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBW.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/OYBS.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OYBE.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OYBN.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OYBW.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/OYBS.gif"));
                 break;
             default:
                 // Si le type d'arme n'est pas valide, utilise une image par défaut
-            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAE.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAN.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAW.gif"));
-            	this.setImage(this.damage.getImage("res/sprites/Objects/ORAS.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/ORAE.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/ORAN.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/ORAW.gif"));
+            	this.setImage(this.game.getImage("res/sprites/Objects/ORAS.gif"));
                 break;
         }
         
@@ -65,11 +67,15 @@ public class Blade extends Sprite {
     
     
 
+	
+
+
+
 	public Kind getKind() {
         return kind;
     }
     
-    public Zelda getDamage() {
+    public int getDamage() {
         return damage;
     }
 
--- objects/Shield.java
+++ objects/Shield.java
@@ -6,4 +6,16 @@ public class Shield {
         SMALL,
         MAGICAL
     }
+    
+    private Kind kind;
+    private int defense; // The amount of damage that can be blocked
+    
+    public Shield(Kind kind) {
+        this.kind = kind;
+        this.defense = (kind == Kind.SMALL) ? 1 : 2; // Set the amount of defense based on the shield type
+    }
+    
+    public int getDefense() {
+        return defense;
+    }
 }
