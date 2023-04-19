520692750b36b3603329c7acbf37ae5ff0840894
zouza-kenza <120374566+zouza-kenza@users.noreply.github.com>
2023-04-19 10:43:56 +0200

    Create Direction.java

---
+++ Direction.java
@@ -0,0 +1,40 @@
+package zelda.enemies;
+
+
+public enum Direction {
+    UP(-1, 0),
+    DOWN(1, 0),
+    LEFT(0, -1),
+    RIGHT(0, 1);
+
+    private final int dx;
+    private final int dy;
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public Direction reverse() {
+        switch (this) {
+            case UP:
+                return DOWN;
+            case DOWN:
+                return UP;
+            case LEFT:
+                return RIGHT;
+            case RIGHT:
+                return LEFT;
+            default:
+                throw new IllegalStateException("Unknown direction: " + this);
+        }
+    }
+}
