bdb3a7adf57c5102c3f863e0f67e475c36abcf31
Helena Santos <38816678+helenafs@users.noreply.github.com>
2023-04-20 18:30:34 +0200

    Add files via upload

--- enemies/AbstractEnemy.java
+++ enemies/AbstractEnemy.java
@@ -1,114 +1,213 @@
 package zelda.enemies;
 
+
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 import com.golden.gamedev.object.AnimatedSprite;
+import com.golden.gamedev.object.CollisionManager;
+import com.golden.gamedev.object.Sprite;
 import com.golden.gamedev.object.SpriteGroup;
+import com.golden.gamedev.object.Timer;
+import com.golden.gamedev.object.collision.AdvanceCollisionGroup;
+import com.golden.gamedev.object.collision.CollisionShape;
 
+import zelda.Link;
 import zelda.Orientation;
 import zelda.Player;
 import zelda.Zelda;
 import zelda.scenary.Board;
+import zelda.scenary.Tile;
 
 import com.golden.gamedev.Game;
+import com.golden.gamedev.object.collision.CollisionBounds;
+
+public abstract class AbstractEnemy extends AnimatedSprite{
+	
+		private static final long serialVersionUID = 1L;
+		private int health;
+		private int damage;
+		private Direction direction;
+		public Zelda game;
+		private Timer figth;
+		
+		private int attackRange;
+	    private int points;
+
+		public static final Direction DEFAULT_DIRECTION = Direction.UP;
+		
+		private Timer changeDirectionTimer;
+	    private static final int CHANGE_DIRECTION_INTERVAL = 500; // in milliseconds
+		
+	    private SpriteGroup objectsGroup;
+	    
+	    Board board; 
+	    
+	    protected SpriteGroup enemies_SGroup;
 
 
-public abstract class AbstractEnemy extends AnimatedSprite {
+		public void Enemy(Zelda game, int startX, int startY, int startHealth, int startDamage) {
+		
+		   
+		}
+		
+		//abstract
+		protected abstract void initResources();		
 
-    private static final long serialVersionUID = 1L;
-    private int health;
-    private int damage;
-    private Direction direction;
-    private Zelda game;
-    public static final Direction DEFAULT_DIRECTION = Direction.UP;
+		
+		public void setObjectsGroup(SpriteGroup objectsGroup) {
+		    this.objectsGroup = objectsGroup;
+		}
 
-    public Enemy(Zelda game, int startX, int startY, int startHealth, int startDamage) {
-        this.setLocation(startX, startY);
-        health = startHealth;
-        damage = startDamage;
-        this.game = game;
-        this.direction = Enemy.DEFAULT_DIRECTION;
-        this.initResources();
-    }
+		public void setBoard(Board board) {
+			
+	    }
+		
+		// Retourne la board sur laquelle est l'enemi
+			public Board getBoard() {
+				System.out.println("Enemy sur la board "+this.board.getX()+" "+this.board.getY());
+				return this.board;
+			}
+			
+			// Retourne true si l'enemy est sur la board 
+			public boolean isOnBoard(Board board) {
+				return this.board == board;
+			}
 
-    // La méthode initResources() est une méthode abstraite car chaque ennemi devra charger
-    // ses propres images de ressources.
-    protected abstract void initResources();
 
-    public void setBoard(Board board) {
-        SpriteGroup link = new SpriteGroup("LINK SPRITE GROUPE");
-        link.add(this);
-        this.manager.setCollisionGroup(link, board.getForeground());
-    }
+		int directionIndex = 0;
+	   
+		private double previousX;
+		private double previousY;
 
-    // La méthode update() est une méthode abstraite car chaque ennemi aura son propre comportement
-    // de mise à jour.
-    public abstract void update(long elapsedTime);
+		private void undoMove() {
+		    setLocation(previousX, previousY);
+		}
+		
+	    public void update(long elapsedTime) {
+	    
+	    }
 
-    // La méthode walk() est une méthode abstraite car chaque ennemi aura son propre comportement
-    // de déplacement.
-    public abstract void walk(Orientation direction);
+	    public void walk(Direction direction) {
+	    	double randomNum = (new Random().nextDouble() * 0.15);
+	    	
+	    	switch (direction) {
+	        case UP:
+	            this.setSpeed(0, -randomNum);
+	            break;
+	        case DOWN:
+	            this.setSpeed(0, randomNum);
+	            break;
+	        case LEFT:
+	            this.setSpeed(-randomNum, 0);
+	            break;
+	        case RIGHT:
+	            this.setSpeed(randomNum, 0);
+	            break;
+	    }
+	}		
 
-    public void attack() {
-        // Code pour gérer l'attaque de l'ennemi
-    }
+	    public int getAttackRange() {
+	        return attackRange;
+	    }
 
-    public void takeDamage(int damage) {
-        health -= damage;
-    }
+	    public int getPoints() {
+	        return points;
+	    }
 
-    public boolean detectPlayer(Player player, int distance) {
-        double playerX = player.getX();
-        double playerY = player.getY();
-        double deltaX = playerX - this.getX();
-        double deltaY = playerY - this.getY();
-        double distanceFromPlayer = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-        return distanceFromPlayer <= distance;
-    }
+		public void attack() {
+		    // Code to handle the enemy's attack
+			int damage = this.damage; 
+			this.game.getGameLink().takeDamage(damage);
+			System.out.println("Link dit: ouch");
+		}
 
-    public void attacked(Player player) {
-        double playerX = player.getX();
-        double playerY = player.getY();
-        double deltaX = playerX - this.getX();
-        double deltaY = playerY - this.getY();
-        double angle = Math.atan2(deltaY, deltaX);
-        angle = Math.toDegrees(angle);
-        if (angle < 0) {
-            angle += 360;
-        }
-        if (angle >= 45 && angle < 135) {
-            direction = Direction.UP;
-        } else if (angle >= 135 && angle < 225) {
-            direction = Direction.LEFT;
-        } else if (angle >= 225 && angle < 315) {
-            direction = Direction.DOWN;
-        } else {
-            direction = Direction.RIGHT;
-        }
-        double moveDistance = 50;
-        double moveX = Math.cos(Math.toRadians(angle)) * moveDistance;
-        double moveY = Math.sin(Math.toRadians(angle)) * moveDistance;
-        move(-1 * (int) moveX, -1 * (int) moveY);
-    }
+		
+		public void takeDamage(int damage) {	
+		    health -= damage;
+		    this.game.playSound("res/sounds/LOZ_Kill.wav"); 
+		    if (health <= 0) {
+	            setActive(false);
+	            System.out.println("Tu as tué l'ennemi");
+	            this.game.playSound("res/sounds/LOZ_Kill.wav"); 
+		    }
+	            
+		}
+		
+		public boolean isAlive() {
+			return this.health > 0;
+		}
 
-    public int getHealth() {
-        return health;
-    }
+		// new method to detect the player within a certain distance from the enemy
+		public boolean detectPlayer(Player player, int distance) {
+		    double playerX = player.getX();
+		    double playerY = player.getY();
+		    double deltaX = playerX - this.getX();
+		    double deltaY = playerY - this.getY();
+		    double distanceFromPlayer = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+		    return distanceFromPlayer <= distance;
+		}
+		
+		// new method to define the enemy's behavior when attacked
+		public void attacked(Player player) {
+		    // Make the enemy face the player
+		    double playerX = player.getX();
+		    double playerY = player.getY();
+		    double deltaX = playerX - this.getX();
+		    double deltaY = playerY - this.getY();
+		    double angle = Math.atan2(deltaY, deltaX);
+		    angle = Math.toDegrees(angle);
+		    if (angle < 0) {
+		        angle += 360;
+		    }
+		    if (angle >= 45 && angle < 135) {
+		        direction = Direction.UP;
+		    } else if (angle >= 135 && angle < 225) {
+		        direction = Direction.LEFT;
+		    } else if (angle >= 225 && angle < 315) {
+		        direction = Direction.DOWN;
+		    } else {
+		        direction = Direction.RIGHT;
+		    }
 
-    public int getDamage() {
-        return damage;
-    }
+		    // Move the enemy away from the player
+		    double moveDistance = 50; // distance to move away from player
+		    double moveX = Math.cos(Math.toRadians(angle)) * moveDistance;
+		    double moveY = Math.sin(Math.toRadians(angle)) * moveDistance;
+		    move(-1 * (int) moveX, -1 * (int) moveY);
+		}
 
-    public Direction getDirection() {
-        return direction;
-    }
+		public int getHealth() {
+		    return health;
+		}
 
-    public void takeDamage(Object damage2) {
-        // TODO Auto-generated method stub
-    }
+		public int getDamage() {
+		    return damage;
+		}
 
-    // La méthode render() est une méthode abstraite car chaque ennemi aura son propre
-    // comportement d'affichage.
-    public abstract void render(Graphics2D g);
+		public Direction getDirection() {
+		    return direction;
+		}
+
+
+		//abstract
+		  public abstract void render(Graphics2D g);
+		  
+		  
+		// Add the following method to change the enemy's direction
+	    private void changeDirection() {
+	        directionIndex++;
+	        directionIndex = directionIndex % Direction.values().length; // increment direction index
+	        direction = Direction.values()[directionIndex];
+	        this.walk(direction);
+	    }
+	    
+		
+	
+
+
 }
